d06d41e881567e147ac756c7aaaf97b3242f2c1f
JulienG <juliengilbert.dev@gmail.com>
2023-04-17 15:21:21 +0200

    Ajout du fichier gitignore

--- Les_petits_plats/script/index.js
+++ Les_petits_plats/script/index.js
@@ -3,3 +3,17 @@ import {initNominal} from "./functions/nominal.js";
 
 initDropdown();
 initNominal();
+
+
+/*
+"scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "sass": "sass --watch ./sass/main.scss:./public/css/style.css"},
+
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "normalize.css": "^8.0.1"
+  },
+*/
+
